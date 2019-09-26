8d7af4fc28b85f14dcaea90428a9c72900a91c7b
daisukiyo <tk.rain09@gmail.com>
2019-09-25 20:19:15 -0700

    added 938

---
+++ leetcode/938/range-sum-of-bst.py
@@ -0,0 +1,23 @@
+'''
+given the root node of a bst
+return the sum of values of all nodes between two values
+
+input: root = [10, 5, 15, 3, 7, null, 18], L = 7, R = 15
+output: 32
+'''
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def rangeSumBST(self, root: TreeNode, L: int, R: int) -> int:
+        if not root:
+            return 0
+        elif root.val < L:
+            return self.rangeSumBST(root.right, L, R)
+        elif root.val > R:
+            return self.rangeSumBST(root.left, L, R)
+        return root.val + self.rangeSumBST(root.left, L, R) + self.rangeSumBST(root.right, L, R)
